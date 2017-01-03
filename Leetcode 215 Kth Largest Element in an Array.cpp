83d73cf0c4d3f1adce7b4a4fd729c82e9c86da3b
Chao Xin <xinchaoxt@gmail.com>
2017-01-03 20:13:01 +0800

    QuickSort & Heap & BFPTR

--- Leetcode 215 Kth Largest Element in an Array.cpp
+++ Leetcode 215 Kth Largest Element in an Array.cpp
@@ -158,113 +158,65 @@ private:
 }
 
 // BFPRT version : Worse O(n) from http://blog.csdn.net/acdreamers/article/details/44656295
-#include <iostream>  
-#include <string.h>  
-#include <stdio.h>  
-#include <time.h>  
-#include <algorithm>  
-   
-using namespace std;  
-const int N = 10005;  
-   
-int a[N];  
-   
-//插入排序  
-void InsertSort(int a[], int l, int r)  
-{  
-    for(int i = l + 1; i <= r; i++)  
-    {  
-        if(a[i - 1] > a[i])  
-        {  
-            int t = a[i];  
-            int j = i;  
-            while(j > l && a[j - 1] > t)  
-            {  
-                a[j] = a[j - 1];  
-                j--;  
-            }  
-            a[j] = t;  
-        }  
-    }  
-}  
-   
-//寻找中位数的中位数  
-int FindMid(int a[], int l, int r)  
-{  
-    if(l == r) return a[l];  
-    int i = 0;  
-    int n = 0;  
-    for(i = l; i < r - 5; i += 5)  
-    {  
-        InsertSort(a, i, i + 4);  
-        n = i - l;  
-        swap(a[l + n / 5], a[i + 2]);  
-    }  
-   
-    //处理剩余元素  
-    int num = r - i + 1;  
-    if(num > 0)  
-    {  
-        InsertSort(a, i, i + num - 1);  
-        n = i - l;  
-        swap(a[l + n / 5], a[i + num / 2]);  
-    }  
-    n /= 5;  
-    if(n == l) return a[l];  
-    return FindMid(a, l, l + n);  
-}  
-   
-//寻找中位数的所在位置  
-int FindId(int a[], int l, int r, int num)  
-{  
-    for(int i = l; i <= r; i++)  
-        if(a[i] == num)  
-            return i;  
-    return -1;  
-}  
-   
-//进行划分过程  
-int Partion(int a[], int l, int r, int p)  
-{  
-    swap(a[p], a[l]);  
-    int i = l;  
-    int j = r;  
-    int pivot = a[l];  
-    while(i < j)  
-    {  
-        while(a[j] >= pivot && i < j)  
-            j--;  
-        a[i] = a[j];  
-        while(a[i] <= pivot && i < j)  
-            i++;  
-        a[j] = a[i];  
-    }  
-    a[i] = pivot;  
-    return i;  
-}  
-   
-int BFPTR(int a[], int l, int r, int k)  
-{  
-    int num = FindMid(a, l, r);    //寻找中位数的中位数  
-    int p =  FindId(a, l, r, num); //找到中位数的中位数对应的id  
-    int i = Partion(a, l, r, p);  
-   
-    int m = i - l + 1;  
-    if(m == k) return a[i];  
-    if(m > k)  return BFPTR(a, l, i - 1, k);  
-    return BFPTR(a, i + 1, r, k - m);  
-}  
-   
-int main()  
-{  
-    int n, k;  
-    scanf("%d", &n);  
-    for(int i = 0; i < n; i++)  
-        scanf("%d", &a[i]);  
-    scanf("%d", &k);  
-    printf("The %d th number is : %d\n", k, BFPTR(a, 0, n - 1, k));  
-    for(int i = 0; i < n; i++)  
-        printf("%d ", a[i]);  
-    puts("");  
-    return 0;  
-}  
+
+class Solution {
+public:
+    void InsertSort(vector<int>& nums, int l, int r){
+        for(int i = l + 1; i <= r; ++ i){
+            if(nums[i - 1] < nums[i]){
+                int tmp = nums[i], j = i;
+                while(j > l && nums[j - 1] < tmp){
+                    nums[j] = nums[j - 1];
+                    -- j;
+                }
+                nums[j] = tmp;
+            }
+        }
+    }
+    int FindMid(vector<int> &nums, int l, int r){
+        if(l == r) return nums[l];
+        int i = 0, n = 0;
+        for(i = l; i < r - 5; i += 5){
+            InsertSort(nums, i, i + 4);
+            n = i - l;
+            swap(nums[l + n / 5], nums[i + 2]);
+        }
+        int len = r - i + 1;
+        if(len > 0){
+            InsertSort(nums, i, i + len - 1);
+            n = i - l;
+            swap(nums[l + n / 5], nums[i + len / 2]);
+        }
+        return FindMid(nums, l, l + n / 5);
+    }
+    int FindID(vector<int> &nums, int l, int r, int n){
+        for(int i = l; i <= r; ++ i)
+            if(nums[i] == n) return i;
+        return -1;
+    }
+    int Partition(vector<int> &nums, int l, int r, int p){
+        swap(nums[p], nums[l]);
+        int i = l, j = r, pivot = nums[l];
+        while(i < j){
+            while(nums[j] <= pivot && i < j) -- j;
+            nums[i] = nums[j];
+            while(nums[i] >= pivot && i < j) ++ i;
+            nums[j] = nums[i];
+        }
+        nums[i] = pivot;
+        return i;
+    }
+    int BFPTR(vector<int> &nums, int l, int r, int k){
+        int num = 0, p = 0, i = 0, res = 0;
+        num = FindMid(nums, l, r);
+        p = FindID(nums, l, r, num);
+        i = Partition(nums, l, r, p);
+        res = i - l + 1;
+        if(res == k) return nums[i];
+        if(res > k) return BFPTR(nums, l, i - 1, k);
+        return BFPTR(nums, i + 1, r, k - res);
+    }
+    int findKthLargest(vector<int>& nums, int k) {
+        return BFPTR(nums, 0, nums.size() - 1,  k);
+    }
+};
