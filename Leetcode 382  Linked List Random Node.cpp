d06c369ef50959e243be470677cc69da57fcf25a
Chao Xin <xinchaoxt@gmail.com>
2017-01-15 23:35:22 +0800

    Reservoir Sampling

---
+++ Leetcode 382  Linked List Random Node.cpp
@@ -0,0 +1,57 @@
+/*
+Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+
+Follow up:
+What if the linked list is extremely large and its length is unknown to you? Could you solve this efficiently without using extra space?
+
+Example:
+
+// Init a singly linked list [1,2,3].
+ListNode head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+Solution solution = new Solution(head);
+
+// getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+solution.getRandom();
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ 
+ 
+ // https://en.wikipedia.org/wiki/Reservoir_sampling
+class Solution {
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    ListNode* head;
+    Solution(ListNode* head) {
+        this->head = head;
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        int res = head->val, i = 1;
+        ListNode* node = head;
+        while(node->next){
+            ++ i;
+            node = node->next;
+            int tmp = rand() % i;
+            if(tmp == 0) res = node->val;
+        }
+        return res;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
