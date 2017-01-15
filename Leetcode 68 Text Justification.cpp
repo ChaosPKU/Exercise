3f32f6198977bbd0be740691f607b57a170b1d64
Chao Xin <xinchaoxt@gmail.com>
2017-01-15 14:39:35 +0800

    String

---
+++ Leetcode 68 Text Justification.cpp
@@ -0,0 +1,43 @@
+/*
+Given an array of words and a length L, format the text such that each line has exactly L characters and is fully (left and right) justified.
+
+You should pack your words in a greedy approach; that is, pack as many words as you can in each line. Pad extra spaces ' ' when necessary so that each line has exactly L characters.
+
+Extra spaces between words should be distributed as evenly as possible. If the number of spaces on a line do not divide evenly between words, the empty slots on the left will be assigned more spaces than the slots on the right.
+
+For the last line of text, it should be left justified and no extra space is inserted between words.
+
+For example,
+words: ["This", "is", "an", "example", "of", "text", "justification."]
+L: 16.
+
+Return the formatted lines as:
+[
+   "This    is    an",
+   "example  of text",
+   "justification.  "
+]
+Note: Each word is guaranteed not to exceed L in length.
+*/
+
+
+class Solution {
+public:
+    vector<string> fullJustify(vector<string>& words, int maxWidth) {
+        vector<string> res;
+        for(int i = 0, k, l;i < words.size(); i += k){
+            for(k = 0,l = 0;i + k < words.size() && l + k + words[i + k].size() <= maxWidth; ++ k)
+                l += words[i + k].size();
+            string tmp = words[i];
+            for(int j = 0; j < k - 1; ++ j){
+                int a = (maxWidth - l) / (k - 1), b = (maxWidth - l) % (k - 1);
+                if(i + k >= words.size()) tmp += " ";
+                else tmp += string(a + (j < b) ,' ');
+                tmp += words[i + j + 1];
+            }
+            tmp += string(maxWidth - tmp.size(), ' ');
+            res.push_back(tmp);
+        }
+        return res;
+    }
+};
