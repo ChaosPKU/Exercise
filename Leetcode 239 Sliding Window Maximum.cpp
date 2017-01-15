5d02f14c60085e64b22fdafbe0a3143877c2f776
Chao Xin <xinchaoxt@gmail.com>
2017-01-15 21:24:54 +0800

    Deque

---
+++ Leetcode 239 Sliding Window Maximum.cpp
@@ -0,0 +1,35 @@
+/*
+Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+For example,
+Given nums = [1,3,-1,-3,5,3,6,7], and k = 3.
+
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+Therefore, return the max sliding window as [3,3,5,5,6,7].
+*/
+
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> q;
+        vector<int> res;
+        for(int i = 0;i < nums.size(); ++ i){
+            if(!q.empty() && i - k == q.front()) q.pop_front();
+            while(!q.empty() && nums[i] > nums[q.back()])
+                q.pop_back();
+            q.push_back(i);
+            if(i >= k - 1) res.push_back(nums[q.front()]);
+        }
+        return res;
+    }
+};
+
+
