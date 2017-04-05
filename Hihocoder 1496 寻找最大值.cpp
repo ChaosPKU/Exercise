b843ac90007f3bbf08ae232961ea3df72efc55af
Chao Xin <xinchaoxt@gmail.com>
2017-04-05 12:53:14 +0800

    Math & Bit Manipulation

--- Hihocoder 1496 寻找最大值.cpp
+++ Hihocoder 1496 寻找最大值.cpp
@@ -28,10 +28,60 @@
 */
 
 // 问题的关键在于如何把O(n^2)的复杂度降低到O(nlogn)级别。
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int highest_one_bit(int n){ // 取最高位1
+    n |= (n >> 1);
+    n |= (n >> 2);
+    n |= (n >> 4);
+    n |= (n >> 8);
+    n |= (n >> 16);
+    return n ^ (n >> 1);
+}
 
+void split(const vector<int> &nums, vector<int> &nums0, vector<int> &nums1, int& m){
+    for(int num : nums){
+        if(num & m)
+            nums1.push_back(num);
+        else
+            nums0.push_back(num);
+    }
+}
 
-// 一种思路可以先把所有a&b可能的结果枚举出来，则对相同的a&b，a*b*a&b最大的就是a&b再乘以最大的a和b。但复杂度很高。
+void solve(const vector<int> &nums, long long &r, int &m){
+    if(nums.empty() || !m) return;
+    vector<int> nums0, nums1;
+    split(nums, nums0, nums1, m);
+    if(nums1.empty()) solve(nums0, r, m >>= 1);  // 当前位所有数字均为0，则结果必在nums0中产生
+    else if(nums1.size() == 1){  // 当前位只有一个数字为1，则结果可能在该数字和其他数字中产生
+        for(int num : nums0)
+            r = max(r, (long long)nums1[0] * num * (nums1[0] & num));
+        solve(nums0, r, m >>= 1);
+    }
+    else solve(nums1, r, m >>= 1);  // 当前位存在两个及两个以上数字均为1，则结果必在nums1中产生
+}
+
+int main(){
+    int T, N, M;
+    long long R;
+    scanf("%d", &T);
+    while(T --){
+        scanf("%d", &N);
+        vector<int> nums(N);
+        R = M = 0;
+        for(int i = 0;i < N; ++ i){
+            scanf("%d", &nums[i]);
+            M = max(M, highest_one_bit(nums[i]));
+        }
+        solve(nums, R, M);
+        printf("%lld\n", R);
+    }
+    return 0;
+}
+
+// 另外一种思路可以先把所有a&b可能的结果枚举出来，则对相同的a&b，a*b*a&b最大的就是a&b再乘以最大的a和b。但复杂度很高。
 #include <iostream>
 #include <memory.h>
 using namespace std;
