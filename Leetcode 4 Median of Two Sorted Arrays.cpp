974205719fecd0c52193047cb55ef32210395ee9
Chao Xin <xinchaoxt@gmail.com>
2017-04-12 21:13:50 +0800

    Binary Search & Array

--- Leetcode 4 Median of Two Sorted Arrays.cpp
+++ Leetcode 4 Median of Two Sorted Arrays.cpp
@@ -16,6 +16,32 @@ nums2 = [3, 4]
 The median is (2 + 3)/2 = 2.5
 
 */
+
+// 二分之后答案只可能在nums1[l-1], nums1[l], nums2[k-l], 和nums2[k-l+1]中取
+// 如果总长是奇数，那么中位数median = max(nums1[l-1], nums2[k-l])
+// 如果总长是偶数，那么中位数median = (max(A[l-1], B[k-l]) + min(A[l], B[k-l+1]) / 2.0
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size(), n2 = nums2.size();
+        if(n1 > n2) return findMedianSortedArrays(nums2, nums1);
+        int mlen = (n1 + n2 - 1) / 2, l = 0, r = min(n1, mlen), mid1, mid2, r1, r2;
+        while(l < r){
+            mid1 = (l + r) / 2;
+            mid2 = mlen - mid1;
+            if(nums1[mid1] < nums2[mid2])
+                l = mid1 + 1;
+            else r = mid1;
+        }
+        r1 = max(l > 0 ? nums1[l - 1] : INT_MIN, mlen - l >= 0 ? nums2[mlen - l] : INT_MIN);
+        if((n1 + n2) & 1) return r1 + 0.0;
+        r2 = min(l < n1 ? nums1[l] : INT_MAX, mlen - l + 1 < n2 ? nums2[mlen - l + 1] : INT_MAX);
+        return (r1 + r2) / 2.0;
+    };
+};
+
+
 // From https://discuss.leetcode.com/topic/4996/share-my-o-log-min-m-n-solution-with-explanation
 class Solution {
 public:
